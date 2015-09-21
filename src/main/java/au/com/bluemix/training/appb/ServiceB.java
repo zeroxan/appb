5d6ffffa8f507a814d45a6a305b006c9b3128274
Danny Yee <dannyee@au1.ibm.com>
2015-09-21 00:13:15 -0700

    Updated ServiceB

--- src/main/java/au/com/bluemix/training/appb/ServiceB.java
+++ src/main/java/au/com/bluemix/training/appb/ServiceB.java
@@ -1,8 +1,15 @@
 package au.com.bluemix.training.appb;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+@Stateless
+@LocalBean
+@WebService(serviceName = "ServiceBService", portName = "ServiceBPort", endpointInterface = "au.com.bluemix.training.appb.IServiceB", targetNamespace = "http://appb/ServiceB")
 public class ServiceB implements IServiceB {
 
-	@Override
+
 	public int minus(int left, int right) {
 		// TODO Auto-generated method stub
 		return left - right;
